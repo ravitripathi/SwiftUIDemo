6127d50886c91ca9376c5f00f873ff8baf766cdc
Ravi Tripathi <ravi.tripathi@go-jek.com>
2020-04-19 18:55:50 +0530

    Initial Commit

---
+++ SwiftUIDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIDemo
+//
+//  Created by Ravi Tripathi on 19/04/20.
+//  Copyright © 2020 Ravi Tripathi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
